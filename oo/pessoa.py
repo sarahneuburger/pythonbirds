cda59556d9e8efc9201b8652b031d1e6b05148dc
sarahneuburger <sarahneuburger94@gmail.com>
2020-02-26 21:10:17 -0300

    Criado e removido atributo dinâmico de objeto do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,25 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=25):
+    def __init__(self, *filhos, nome=None, idade=25):
         self.nome = nome #none = nulo#
+        self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f"Olá{id(self)}"
 
 if __name__ == '__main__':
-    p = Pessoa("Henrique")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = "Sarah" #alterar valor do atributo#
-    print(p.nome)
-    print(p.idade)
+    helena = Pessoa(nome="Helena")
+    henrique = Pessoa(helena, nome="Henrique")
+    print(Pessoa.cumprimentar(henrique))
+    print(id(henrique))
+    print(henrique.cumprimentar())
+    print(henrique.nome)
+    print(henrique.idade)
+    for filho in henrique.filhos:
+        print(filho.nome)
+    henrique.sobrenome = "Martins"
+    print(henrique.sobrenome)
+    del henrique.filhos
+    print(henrique.__dict__)
+    print(helena.__dict__)
+

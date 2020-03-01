321e9300211c64faf74b23943518e9c384a566ae
sarahneuburger <sarahneuburger94@gmail.com>
2020-02-29 21:57:18 -0300

    Implementada Direcao

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,16 @@ class Pessoa:
     def cumprimentar(self):
         return f"Olá{id(self)}"
 
+    @staticmethod #Método de classe - decorator#
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f"{cls} - olhos {cls.olhos}"
+
+
+
 if __name__ == '__main__':
     helena = Pessoa(nome="Helena")
     henrique = Pessoa(helena, nome="Henrique")
@@ -29,3 +39,6 @@ if __name__ == '__main__':
     print(henrique.olhos)
     print(helena.olhos)
     print(id(Pessoa.olhos), id(henrique.olhos), id(helena.olhos))
+    print(Pessoa.metodo_estatico(), henrique.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), henrique.nome_e_atributos_de_classe())
+
